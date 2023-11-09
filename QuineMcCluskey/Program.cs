c2d6036c6d5e53e32d554dfb17878fc238186f18
Seppn <Seppn@BIGGERBOTWO>
2023-11-09 20:11:59 +0100

    Projektdateien hinzufügen.

---
+++ QuineMcCluskey/Program.cs
@@ -0,0 +1,15 @@
+﻿List<int> expectedOutputs = new List<int>();
+
+while (true)
+{
+    string? str = Console.ReadLine();
+    if (!int.TryParse(str, out int res))
+        break;
+
+    expectedOutputs.Add(res);
+}
+
+for (int i = 0; i<expectedOutputs.Count; i++)
+{
+    Console.WriteLine(expectedOutputs[i]);
+}
